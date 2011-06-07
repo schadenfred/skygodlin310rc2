ed983bc2af19aa8106be4e4a43fbc50e562b94b2
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-06-06 18:31:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skygodlin::Application.config.secret_token = 'ef751e7b165293ca9f677b31b01e2b6ee7e0ac8a95c5a43bc189e56e9147621395049ed17102dffe49d07bd1663e61b18389d071c8e401ee5f4ccc25748c6b88'
